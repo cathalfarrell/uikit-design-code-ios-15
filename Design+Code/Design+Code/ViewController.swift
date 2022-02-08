b332b43ebd82410669b7a38b2564112cdc805dcf
Cathal Farrell <cathalproductions@gmail.com>
2022-02-08 11:26:19 +0000

    Connected Storyboard CardView to UIViewController class, to add rounded corner and shadow

--- Design+Code/Design+Code/ViewController.swift
+++ Design+Code/Design+Code/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cardView: UIView!
+    @IBOutlet weak var blurView: UIVisualEffectView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        cardView.layer.cornerRadius = 30
+        cardView.layer.cornerCurve = .continuous
+        blurView.layer.cornerRadius = 30
+        blurView.layer.cornerCurve = .continuous
+
+
+        blurView.layer.masksToBounds = true
+
+        //Shadow
+        cardView.layer.shadowColor = UIColor(named: "Shadow")!.cgColor
+        cardView.layer.shadowOpacity = 0.5
+        cardView.layer.shadowOffset = CGSize(width: 0, height: 10)
+        cardView.layer.shadowRadius = 20
     }
-
-
 }
 
